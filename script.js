f877491bc032ec51192b296558d35e35bf6922d0
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-13 21:53:00 +0530

    Complete changing color on hover.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ for(let i = 0; i < 16 * 16; i++) {
 	let div = document.createElement('div');
 	div.setAttribute("class", 'block')
 	container.appendChild(div);
+	div.addEventListener("mouseenter", () => {
+		div.style.backgroundColor = 'aquamarine';
+	})
+	// div.addEventListener("mouseleave", () => {
+	// 	div.style.backgroundColor = 'pink';
+	// })
 }
-
-
