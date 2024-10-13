f5a1d3a95acde2d2ac76cc61752c0f71a2de43a7
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-13 22:35:18 +0530

    Complete variable size grid

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const container = document.querySelector('.container');
+const noOfSq = document.querySelector('.noOfSq');
+let n = 16;
 
-
-//create 16x16 grid
-for(let i = 0; i < 16 * 16; i++) {
-	let div = document.createElement('div');
-	div.setAttribute("class", 'block')
-	container.appendChild(div);
-	div.addEventListener("mouseenter", () => {
-		div.style.backgroundColor = 'aquamarine';
-	})
-	// div.addEventListener("mouseleave", () => {
-	// 	div.style.backgroundColor = 'pink';
-	// })
+//create nxn grid
+for(let j = 0; j < n; j++) {
+	const line = document.createElement('div');
+	line.setAttribute("class", 'line');
+	for(let i = 0; i < n; i++) {
+		let div = document.createElement('div');
+		div.setAttribute("class", 'block')
+		line.appendChild(div);
+		div.addEventListener("mouseenter", () => {
+			div.style.backgroundColor = 'aquamarine';
+		})
+	}
+	container.appendChild(line);
 }
+
+//popup how many squares on button click
+noOfSq.addEventListener('click', () => {
+	let squareNo = prompt("Enter number of squares per side");
+
+})
